bf98f0c6c1dcc64f9eef25960e947c062812a267
Jimpapitas28 <marlendevasquezz88@gmail.com>
2025-01-28 22:29:29 -0600

    Agregar archivos de proyecto.

---
+++ Tabla.aspx
@@ -0,0 +1,57 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Tabla.aspx.cs" Inherits="Table3381768.Tabla" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <asp:Label ID="Label1" runat="server" Font-Bold="true" Text="Employee"></asp:Label>
+            <asp:Table ID="Table1" runat="server" GridLines="Horizontal" Width="233px">
+                <asp:TableRow>
+
+                    <asp:TableCell>
+                        <asp:Label ID="Label2" runat="server" Text="Last Name"></asp:Label>
+                    </asp:TableCell>
+
+                    <asp:TableCell>
+                        <asp:TextBox ID="TxtLastName" runat="server"></asp:TextBox>
+                    </asp:TableCell>
+
+                </asp:TableRow>
+
+                <asp:TableRow>
+
+                    <asp:TableCell>
+                        <asp:Label ID="Label3" runat="server" Text="First Name"></asp:Label>
+                    </asp:TableCell>
+
+                    <asp:TableCell>
+                        <asp:TextBox ID="TxtFirstName" runat="server"></asp:TextBox>
+                    </asp:TableCell>
+
+                </asp:TableRow>
+
+                <asp:TableRow>
+
+                    <asp:TableCell>
+                        <asp:Label ID="Label4" runat="server" Text="Middle Initial"></asp:Label>
+                    </asp:TableCell>
+
+                    <asp:TableCell>
+                        <asp:TextBox ID="TxtMiddleInitial" runat="server" Width="20px"></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+
+            <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" Width="69px" />
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Tabla.aspx.designer.cs
@@ -0,0 +1,107 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace Table3381768
+{
+
+
+    public partial class Tabla
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control Table1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Table Table1;
+
+        /// <summary>
+        /// Control Label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label2;
+
+        /// <summary>
+        /// Control TxtLastName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtLastName;
+
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Control TxtFirstName.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtFirstName;
+
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Control TxtMiddleInitial.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TxtMiddleInitial;
+
+        /// <summary>
+        /// Control btnSave.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnSave;
+    }
+}
